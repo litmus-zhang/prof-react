2f02971e9c43be009185e03fbe1739d0978d5762
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-05-13 07:19:36 +0100

    setting up eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const Home: NextPage = () =>
+{
+  
+  
   return (
     <div className={styles.container}>
       <Head>
